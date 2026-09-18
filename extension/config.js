43769a0f3e0545be64796acf2c870423ff5bf59a
Aditya Bhosale <adityabhosale4388@gmail.com>
2026-09-18 16:46:52 +0530

    backend url update

--- extension/config.js
+++ extension/config.js
@@ -1,2 +1,2 @@
 // When deploying, change this URL and the matching host permission in manifest.json.
-export const API_BASE = 'http://127.0.0.1:3000';
+export const API_BASE = 'https://titlepulse.onrender.com';
